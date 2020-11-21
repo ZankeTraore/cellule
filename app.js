d74c40a87c10b1031259c35bc09870dc2c84b1e9
ZankeTraore <65517760+ZankeTraore@users.noreply.github.com>
2020-11-20 22:01:43 -1200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//window load all website by javaScript created by zanke traore at 20/18/2018
+function OpenFB(){const load="www.facebook.com/id"
+    window.location=load;
+}function OpenGoogle(){const load="www.google.com/id"
+window.location=load;
+}function OpenInst(){const load="www.insta.com/id"
+window.location=load;
+}function OpenTwiter(){const load="www.twiter.com/id"
+window.location=load;
+}function OpenLink(){const load="www.link.com/id"
+window.location=load;
+}
+//window local storage
+function add(){
+    text = customElements;
+}
