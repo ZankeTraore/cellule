e09c334f66c6947f02c7a4b7ed11feb74921ea0a
ZankeTraore <65517760+ZankeTraore@users.noreply.github.com>
2020-11-24 22:32:45 -1200

    Update app.js

--- app.js
+++ app.js
@@ -13,4 +13,34 @@ window.location=load;
 //window local storage
 function add(){
     text = customElements;
-}
+ contextID = "cad92a21-6b0e-48b1-95bc-291bcb1587fb"; 
+    if(contextID = "id/c6=49=2"){}}function getText() {
+    document.getElementById("myDropdown")
+    .classList.toggle("show"); }
+    function filterFunction() {
+ var input, filter, ul, li, a, i;
+input = document.getElementById("myInput");filter = input.value.toUpperCase();
+div = document.getElementById("myDropdown");a = div.getElementsByTagName("a");
+   // div.innerHTML="#";   
+      for (i = 0; i < a.length; i++) {
+    
+      if (a[i].innerHTML.toUpperCase()
+      .indexOf(filter) > -1) {
+
+               a[i].style.display = "";
+            } else {  
+               
+              a[i].style.display="none";
+            }
+        }
+    }function body(){
+        let body =document.getElementById('body');
+        if(body.click){ m = document.getElementById("myDropdown")
+        .classList.toggle("show");
+            if(m.classList.isToggle="show"){
+                document.getElementById("myDropdown")
+                .classList.toggle("hide");
+            }
+          
+        }
+    }
